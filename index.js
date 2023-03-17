4310356ed6f03a4d15f78af668c679cedda9d568
Kvaratskhelia <i.kvaratskhelia@credo.ge>
2023-03-17 17:06:47 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const todoContainer = document.querySelector('.todo-container');
+const input = document.querySelector('input');
+const addBtn = document.querySelector('.fa-plus');
+const footer = document.querySelector('.footer');
+const clearAllBtn = document.querySelector('.clear');
+
+const BASE_URL = 'http://localhost:3000';
